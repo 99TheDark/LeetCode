0076e8b4fcfa28a3d5eb5762f211c8d0585cd269
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-20 21:34:00 -0700

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -5,17 +5,23 @@
 var longestCommonPrefix = function(strs) {
     let prefix = "";
     let idx = 0;
-    outer:
     while(true) {
         let common;
+        let all = true;
         for(let str of strs) {
             let ch = str[idx];
+            if(idx >= str.length) {
+                all = false;
+                break;
+            }
             if(!common) {
                 common = ch;
             } else if(ch != common) {
-                break outer;
+                all = false;
+                break;
             }
         }
+        if(!all) break;
         prefix += common;
         idx++;
     }
